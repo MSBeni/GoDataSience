7d2491d9e5d1df5106bba7d77d0e4628c45d1221
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-05-25 20:07:35 -0400

    Added the prepare data file for applying the ML

---
+++ Section5/prepare_data.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	. "github.com/cdipaolo/goml/base"
+	"io/ioutil"
+	//"regexp"
+	// "strings"
+)
+
+const (
+	TRAIN_DATA_PATH          = "aclImdb/train/"
+	TEST_DATA_PATH           = "aclImdb/test/"
+	POSITIVE_TRAIN_DATA_PATH = TRAIN_DATA_PATH + "/pos/"
+	NEGATIVE_TRAIN_DATA_PATH = TRAIN_DATA_PATH + "/neg/"
+	POSITIVE_TEST_DATA_PATH  = TEST_DATA_PATH + "/pos/"
+	NEGATIVE_TEST_DATA_PATH  = TEST_DATA_PATH + "/neg/"
+)
+
+// struct used in goml
+//type TextDatapoint struct{
+//	X string `json:"x"`
+//	Y unit8  `json:"y"`
+//}
+
+func ReadData(dir string) []string{
+	fileInfo, err := ioutil.ReadDir(dir)
+	if err!= nil{
+		panic(err)
+	}
+	var dataStrings []string
+	for _, file := range fileInfo{
+		bytes, err := ioutil.ReadFile(dir + file.Name())
+		if err!= nil{
+			panic(err)
+		}
+		dataStrings = append(dataStrings, string(bytes))
+	}
+	return dataStrings
+}
+
+// Prepare data in TextDatapoint struct for text classifier
+func GetDataForClassifier() ([]TextDatapoint, []TextDatapoint) {
+	var TrainData []TextDatapoint
+	var TestData []TextDatapoint
+
+	positiveTrainDataStrings := ReadData(POSITIVE_TRAIN_DATA_PATH)
+	negativeTrainDataStrings := ReadData(NEGATIVE_TRAIN_DATA_PATH)
+	positiveTestDataStrings := ReadData(POSITIVE_TEST_DATA_PATH)
+	negativeTestDataStrings := ReadData(NEGATIVE_TEST_DATA_PATH)
+
+	for _, str := range positiveTrainDataStrings {
+		d := TextDatapoint{str, 1}
+		TrainData = append(TrainData, d)
+	}
+	for _, str := range negativeTrainDataStrings {
+		d := TextDatapoint{str, 0}
+		TrainData = append(TrainData, d)
+	}
+	for _, str := range positiveTestDataStrings {
+		d := TextDatapoint{str, 1}
+		TestData = append(TrainData, d)
+	}
+	for _, str := range negativeTestDataStrings {
+		d := TextDatapoint{str, 0}
+		TestData = append(TrainData, d)
+	}
+	return TrainData, TestData
+}
+
+func main() {
+	TrainData, _ := GetDataForClassifier()
+	var CrossValData [][]TextDatapoint
+
+	k := len(TrainData) * 1 / 5
+	fmt.Println("Items in each fold are:", k)
+	for idx := 0; idx < len(TrainData); idx += k {
+		CrossValData = append(CrossValData, TrainData[idx:idx+k])
+		fmt.Println(len(TrainData[idx:idx+k]), len(CrossValData))
+	}
+}
